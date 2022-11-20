b308fc4ec6469961835fdef13ce11588e4adbe5a
Marcelaqprz <43529909+Marcelaqprz@users.noreply.github.com>
2022-11-20 10:10:42 -0600

    Functional APP with Amplify

---
+++ lib/amplifyconfiguration.dart
@@ -0,0 +1,82 @@
+const amplifyconfig = ''' {
+    "UserAgent": "aws-amplify-cli/2.0",
+    "Version": "1.0",
+    "api": {
+        "plugins": {
+            "awsAPIPlugin": {
+                "aquaproject": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://nmun2jzgr5g65apq6ugo5oqyoq.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-rbmvrzbq2vbwxkw5xre5ryxw6i"
+                },
+                "aquaproject-staging": {
+                    "endpointType": "GraphQL",
+                    "endpoint": "https://nmun2jzgr5g65apq6ugo5oqyoq.appsync-api.us-east-1.amazonaws.com/graphql",
+                    "region": "us-east-1",
+                    "authorizationType": "API_KEY",
+                    "apiKey": "da2-rbmvrzbq2vbwxkw5xre5ryxw6i"
+                }
+            }
+        }
+    },
+    "auth": {
+        "plugins": {
+            "awsCognitoAuthPlugin": {
+                "UserAgent": "aws-amplify-cli/0.1.0",
+                "Version": "0.1.0",
+                "IdentityManager": {
+                    "Default": {}
+                },
+                "AppSync": {
+                    "Default": {
+                        "ApiUrl": "https://nmun2jzgr5g65apq6ugo5oqyoq.appsync-api.us-east-1.amazonaws.com/graphql",
+                        "Region": "us-east-1",
+                        "AuthMode": "API_KEY",
+                        "ApiKey": "da2-rbmvrzbq2vbwxkw5xre5ryxw6i",
+                        "ClientDatabasePrefix": "aquaproject_API_KEY"
+                    }
+                },
+                "CredentialsProvider": {
+                    "CognitoIdentity": {
+                        "Default": {
+                            "PoolId": "us-east-1:0fa62323-1b0e-4f9b-b0fd-0e9ebc75d609",
+                            "Region": "us-east-1"
+                        }
+                    }
+                },
+                "CognitoUserPool": {
+                    "Default": {
+                        "PoolId": "us-east-1_S8BiBFEe4",
+                        "AppClientId": "f3lj48a178ltoqaemfsb0g89j",
+                        "Region": "us-east-1"
+                    }
+                },
+                "Auth": {
+                    "Default": {
+                        "authenticationFlowType": "USER_SRP_AUTH",
+                        "socialProviders": [],
+                        "usernameAttributes": [
+                            "EMAIL"
+                        ],
+                        "signupAttributes": [
+                            "EMAIL"
+                        ],
+                        "passwordProtectionSettings": {
+                            "passwordPolicyMinLength": 8,
+                            "passwordPolicyCharacters": []
+                        },
+                        "mfaConfiguration": "OFF",
+                        "mfaTypes": [
+                            "SMS"
+                        ],
+                        "verificationMechanisms": [
+                            "EMAIL"
+                        ]
+                    }
+                }
+            }
+        }
+    }
+}''';
